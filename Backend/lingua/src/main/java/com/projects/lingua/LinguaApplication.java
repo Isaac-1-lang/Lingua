f6c1d3ab5377a32c69cbbe48146f8b68c1548eb2
NIYOBYOSE-Isaac-Precieux <isaprecieux112@gmail.com>
2026-01-19 04:50:59 +0200

    Initial commit to the backend

---
+++ Backend/lingua/src/main/java/com/projects/lingua/LinguaApplication.java
@@ -0,0 +1,13 @@
+package com.projects.lingua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinguaApplication.class, args);
+    }
+
+}
